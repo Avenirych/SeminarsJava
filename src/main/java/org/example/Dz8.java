975edebeb5d3ebeb8aeaf3203e16a03ad4af9a5b
Sergey <sergeileonov@inbox.ru>
2023-03-09 22:44:22 +0000

    Dz8

---
+++ src/main/java/org/example/Dz8.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Arrays;
+import java.util.Scanner;
+//Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+// и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+public class Dz8 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Input length of array: ");
+            int num = scan.nextInt();
+
+            int[][] array = new int[num][num];
+            for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    if (i == j | j == num - i - 1)
+                        array[i][j] = 1;
+                }
+                System.out.println(Arrays.toString(array[i]));
+            }
+        }
+    }
