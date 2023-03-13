32eeecfeb4310b8939b80b89c03771fcb12c98e4
Sergey <sergeileonov@inbox.ru>
2023-03-13 18:44:02 +0000

    Dz Second day

---
+++ src/main/java/org/example/Dz2_2.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+//Создать файл с названием file.txt. Если файл уже есть, то создавать не надо Записать в него Слово "TEXT" 100 раз
+public class Dz2_2 {
+           public static void main(String[] args) throws FileNotFoundException {
+            File file = new File("file.txt");
+            PrintWriter text = new PrintWriter(file);
+            text.println("TEXT ".repeat(100));
+            text.close();
+        }
+    }
