4de5f3dec8eef316853623cfad5d02389c3c4f9d
Sergey <sergeileonov@inbox.ru>
2023-03-27 14:36:35 +0100

    Dz End

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person[] people = new Person[]{new Person("Petr", "Bashiriv", 47, "M", "89061234567"),
+                new Person("Nikolai", "Ivanov", 31, "M", "89061234568"),
+                new Person("Katja", "Novicova", 22, "F", "89061234569"),
+                new Person("Jorg", "Simenon", 18, "M", "89061234560"),
+                new Person("Vera", "Brejneva", 16, "F", "89061234566"),
+
+        };
+
+        for (Person person : people) {
+            if (person.getAge() > 20) {
+                System.out.println(person);
+            }
+        }
+
+        for (Person person : people) {
+            if (person.getGender() == "M") {
+                person.sayHello();
+            }
+        }
     }
 }
